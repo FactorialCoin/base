707db4a380ff73a6a7d072290150b62ff30b18ec
FactorialCoin <38966034+FactorialCoin@users.noreply.github.com>
2018-05-17 00:39:13 +0200

    Add files via upload
    gClient patch.

--- perl/site/lib/gclient.pm
+++ perl/site/lib/gclient.pm
@@ -117,9 +117,7 @@ sub openconnection {
 
   # Set autoflush on socket
   $sock->autoflush(1);
-  #select($self->{socket}); 
-  binmode($self->{socket}); 
-  #$|=1;
+  select($self->{socket}); binmode($self->{socket}); $|=1;
 
   # Set output to console
   select(STDOUT); binmode(STDOUT); $|=1;
@@ -472,56 +470,62 @@ sub website_event {
   if (($command eq 'quit') || ($command eq 'error')) {
     $client->{iquit}=1
   } elsif ($command eq 'input') {
-    $client->{hbuffer}.=$data;
     if ($client->{readheader}) {
-      if ($client->{hbuffer} =~ /\r\n\r\n/) {
-        my ($hdata,@cdata)=split(/\r\n\r\n/,$client->{hbuffer});
-        $client->{hbuffer}=join("\r\n\r\n",@cdata);
-        foreach my $line (split(/\r\n/,$hdata)) {
-          processheader($client,$line)
-        }
+      if (length($data) == 0) {
         $client->{readheader}=0;
+        # data to be expected?
         if (defined $client->{header}{'Content-Length'}) {
           $client->{httplength}=$client->{header}{'Content-Length'};
           if (!$client->{httplength}) {
-            $client->{iquit}=1; return
+            $client->{iquit}=1
           }
         } elsif ((defined $client->{header}{'Transfer-Encoding'}) && ($client->{header}{'Transfer-Encoding'} =~ /chunked/i)) {
           $client->{chunked}=1
         } elsif ((defined $client->{header}{'Content-Encoding'}) && ($client->{header}{'Content-Encoding'} =~ /chunked/i)) {
           $client->{chunked}=1
-        }        
+        }
+      } else {
+        processheader($client,$data);
       }
-    }
-    if (!$client->{readheader}) {
-      if (length($client->{hbuffer}) >= $client->{httplength}) {
-        # prevent exploits
-        $client->{hbuffer}=substr($client->{hbuffer},0,$client->{httplength});
-        if ($client->{chunked}) {
-          my $mode=1; my $pos=0; my $size=0; my $read="";
-          while ($pos < length($client->{hbuffer})) {
-            if ($mode == 1) {
-              if (substr($client->{hbuffer},$pos,2) eq "\r\n") {
-                if ($read =~ /[^a-fA-F0-9]/) {
-                  $client->{error}="Corrupted chunk-size in content"; $client->{iquit}=1; return
-                }
-                $size=hex($read); $read=""; $mode=2; $pos+=2
-              } else {
-                $read.=substr($client->{hbuffer},$pos,1); $pos++
-              }
+    } else {
+      if ($client->{chunked}) {
+        if (!$client->{chunk}) {
+          if (length($data) == 0) {
+            $client->{iquit}=1
+          } else {
+            my $len=length($data);
+            if ($len<6) {
+              $client->{chunk}=hex($data);
             } else {
-              $client->{content}.=substr($client->{hbuffer},$pos,$size);
-              $pos+=$size;
-              if (substr($client->{hbuffer},$pos,2) ne "\r\n") {
-                $client->{error}="Corrupted chunked data in content"; $client->{iquit}=1; return
-              }
-              $pos+=2; $mode=1
+              $client->{chunk}=0
+            }
+            if ($client->{chunk} == 0) {
+              $client->{iquit}=1
             }
           }
-        } else {        
-          $client->{content}=$client->{hbuffer};
+        } else {
+          if (length($data)+2<=$client->{chunk}) { $data.="\r\n" }
+          $client->{chunkbuf}.=$data;
+          $client->{chunk}-=length($data);
+          if ($client->{chunk}<=0) {
+            $client->{content}.=$client->{chunkbuf};
+            $client->{chunkbuf}=""; $client->{chunk}=0;
+          }
         }
-        $client->{iquit}=1;
+      } else {        
+        $client->{content}.=$data;
+        $client->{dataread}+=length($data);
+        if ($client->{httplength}) {
+          if ($client->{dataread}+2<=$client->{httplength}) {
+            # is the 'enter' data or not?
+            $client->{content}.="\r\n"; $client->{dataread}+=2
+          }
+          if ($client->{dataread}+2>=$client->{httplength}) {
+            $client->{iquit}=1;
+          }
+        } else {
+          $client->{content}.="\r\n"; $client->{dataread}+=2
+        }
       }
     }
   }
@@ -556,23 +560,28 @@ sub website {
   my $head=join("\r\n",@header)."\r\n\r\n";
   my $client;
   if ($proxy) {
-    $client=tcpip($proxyinfo->{host},$proxyinfo->{port},0,\&website_event,$proxyinfo->{ssl},0,5);
+    $client=tcpip($proxyinfo->{host},$proxyinfo->{port},0,\&website_event,$proxyinfo->{ssl},1,5);
   } else {
-    $client=tcpip($info->{host},$info->{port},0,\&website_event,$info->{ssl},0,5);
+    $client=tcpip($info->{host},$info->{port},0,\&website_event,$info->{ssl},1,5);
   }
   $client->out($head);
   $client->outburst();
   #$client->{debug}=1;
   $client->{readheader}=1;
-  $client->{hbuffer}="";
+  $client->{gotheader}=0;
   $client->{header}={};
+  $client->{content}="";
   $client->{httplength}=0;
   $client->{chunked}=0;
-  $client->{content}="";
-  $client->{iquit}=0;
+  $client->{chunk}=0;
+  $client->{chunkbuf}="";
+  $client->{dataread}=0;
   $client->{timedout}=0;
+  $client->{dd}="";
+  $client->{iquit}=0;
   my $tm=gettimeofday();
   while (!$client->{iquit}) {
+    usleep(10000);
     $client->takeloop();
     if (gettimeofday()-$tm>5) {
       $client->{timedout}=1;
@@ -782,7 +791,7 @@ sub handlews {
   if ($type eq 'close') {
     utf8::decode($msg);
     my $code=(ord(substr($msg,0,1))<<8)+ord(substr($msg,1,1));
-    $msg=substr($msg,2); if (!$msg) { $msg="Quit" }
+    $msg=substr($msg,2);
     &$caller($self,'quit',"$code $msg");
     if ($self->{verbose}) {
       print STDOUT "\nWebSocket server has closed the connection: $code $msg\n"
@@ -805,8 +814,7 @@ sub wsmsg {
 
 sub wsquit {
   my ($self,$msg) = @_;
-  $self->wsout($msg,'close');
-  $self->outburst()
+  $self->wsout($msg,'close')
 }
 
 sub wsout {
